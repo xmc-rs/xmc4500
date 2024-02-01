c7d600dfe6747b871b5d4dc9a93d6e133d1962e5
lucasbrendel <1765755+lucasbrendel@users.noreply.github.com>
2024-02-01 05:20:02 +0000

    feat: updating from newer svd2rust

--- src/scu_reset/prstat2.rs
+++ src/scu_reset/prstat2.rs
@@ -257,5 +257,5 @@ impl crate::RegisterSpec for PRSTAT2_SPEC {
 impl crate::Readable for PRSTAT2_SPEC {}
 #[doc = "`reset()` method sets PRSTAT2 to value 0xf6"]
 impl crate::Resettable for PRSTAT2_SPEC {
-    const RESET_VALUE: Self::Ux = 0xf6;
+    const RESET_VALUE: u32 = 0xf6;
 }
